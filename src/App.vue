62eb1d7d889e6a67d831c4e0f656ea6aaef81a4d
D. Larson <github@epilektric.com>
2018-04-30 14:38:25 -0500

    Player controls mostly done

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
         <b-container>
             <b-row>
                 <template v-for="(videoId, index) in videoList">
-                    <tv v-bind:videoId="videoId"></tv>
+                    <tv :videoId="videoId" :videoList="videoList" @event="handler"></tv>
                     <div v-if="(index + 1) % 3 == 0" class="w-100"></div>
                 </template>
             </b-row>
@@ -21,18 +21,19 @@ export default {
             return index % 3 ? true : false
         },
         videoList: function(){
+            // TODO: Make a form so users can add a video or video list
             var vList = [
                 'c8W-auqg024',    // Garlic bread to the edge of space
                 'KhaLiiSUvAw',    // Ferrari Enzo Tour
                 '1U-cgn3cEGA',    // Best of Rodney Mullen
-                '_bkX5VkZg8U',    // Ken Block Gymkhana 9
-                'W4afbDBWINU',    // Gold Robot Dancers
-                '05hTBAOnDQE',    // Cyanide & Happiness: Le Telepathe
-                'ayf1sYiNLhQ',    // TV Test Pattern
-                'ZApMxLmpIhQ',    // Habibi Lal Belly Dance
-                'W4YcRYzeakA',    // Moving a bed with a bike
-                'fNp5JZ2vA8o',    // 11 Year Old Dubstep Dance
-                'IvUU8joBb1Q'    // Wintergatan Marble Music Machine
+                // '_bkX5VkZg8U',    // Ken Block Gymkhana 9
+                // 'W4afbDBWINU',    // Gold Robot Dancers
+                // '05hTBAOnDQE',    // Cyanide & Happiness: Le Telepathe
+                // 'ayf1sYiNLhQ',    // TV Test Pattern
+                // 'ZApMxLmpIhQ',    // Habibi Lal Belly Dance
+                // 'W4YcRYzeakA',    // Moving a bed with a bike
+                // 'fNp5JZ2vA8o',    // 11 Year Old Dubstep Dance
+                // 'IvUU8joBb1Q'    // Wintergatan Marble Music Machine
             ]
             var tvCount = (vList.length > 8 ? 8 : vList.length)
             var defaultVideo = 'spYwizUnxCI'   // TV Test Pattern
@@ -44,9 +45,22 @@ export default {
             return vList.sort( function() { return 0.5 - Math.random() } );
         }
     },
+    methods: {
+        handler(params) {
+            if('inc' in params) this.incrementVideo(params)
+        },
+        incrementVideo(params) {
+            var index = this.videoList.indexOf(params.videoId)
+            var length = this.videoList.length
+            var newIndex = 0
+            if(index + params.inc >= 0 && index + params.inc <= length-1) newIndex = index + params.inc
+            return this.videoList[newIndex]
+        }
+    },
     components: {
         tv
-    }
+    },
+
 }
 </script>
 
--- src/components/Television.vue
+++ src/components/Television.vue
@@ -1,7 +1,13 @@
 <template>
     <b-col class="television" md v-autoheight="95">
-        <youtube class="player" width="300" height="169" v-autoheight="83" :video-id="videoId" :player-vars="playerVars" @playing="playing"></youtube>
-        <div class="frame"></div>
+        <youtube style="margin:1em;" class="player" ref="youtube" width="300" height="169" v-autoheight="83" :video-id="videoId" :player-vars="playerVars" @playing="playing"></youtube>
+        <div class="frame">
+            <div class="controls">
+                <div class="button play-pause" v-on:click="playPauseVideo"></div>
+                <div class="button next" v-on:click="nextVideo"></div>
+                <div class="button previous" v-on:click="previousVideo"></div>
+            </div>
+        </div>
     </b-col>
 </template>
 
@@ -14,16 +20,61 @@ export default {
         return {
             playerVars: {
                 autoplay: 0
-            }
+            },
+            isPlaying: false,
+            mutableVideoId: null
         }
     },
     props: {
-        videoId: ''
+        videoId: null,
+        videoList: null
     },
     methods: {
+        // REF: https://github.com/anteriovieira/vue-youtube/blob/master/src/vue-youtube.js
+
         playing() {
-            console.log('we are watching!!!')
+            this.isPlaying = true
+        },
+        playPauseVideo() {
+            if(this.isPlaying == false) {
+                this.player.playVideo()
+                this.isPlaying = true
+            } else {
+                this.player.pauseVideo()
+                this.isPlaying = false
+            }
+        },
+        nextVideo() {
+            var array = this.$props.videoList
+            var i = array.indexOf(this.mutableVideoId)
+            var newVideoId = array[i==array.length-1 ? 0 : i+1]
+            this.mutableVideoId = newVideoId
+            return this.updateVideo(newVideoId)
+        },
+        previousVideo() {
+            var array = this.$props.videoList
+            var i = array.indexOf(this.mutableVideoId)
+            var newVideoId = array[i==0 ? array.length-1 : i-1]
+            this.mutableVideoId = newVideoId
+            return this.updateVideo(newVideoId)
+        },
+        updateVideo(videoId) {
+            console.log(videoId)
+            this.$refs.youtube.updatePlayer(videoId)
+            if(this.isPlaying) {
+                this.player.playVideo()
+            } else {
+                this.isPlaying = false
+            }
         }
+    },
+    computed: {
+        player () {
+            return this.$refs.youtube.player
+        }
+    },
+    mounted() {
+        this.mutableVideoId = this.$props.videoId
     }
 }
 </script>
@@ -46,5 +97,38 @@ export default {
 }
 .player {
     margin: 1em; /* This isn't working. */
+}
+
+.controls .button {
+    cursor: pointer;
+}
+.controls .play-pause {
+    position: absolute;
+    top: 0;
+    right: 4em;
+    bottom: 0;
+    left: 0;
+}
+.controls .next,
+.controls .previous {
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 20em;
+}
+.controls .next {
+    position: absolute;
+    top: 6.4em;
+    right: 0;
+    bottom: 4em;
+    left: 20em;
+}
+.controls .previous {
+    position: absolute;
+    top: 8.6em;
+    right: 0;
+    bottom: 1.5em;
+    left: 20em;
 }
 </style>
